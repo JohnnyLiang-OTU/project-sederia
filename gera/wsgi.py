5bb8adc132d623c613ec5373a4bee98559a04e70
Johnny <johnny.liang@ontariotechu.net>
2024-12-18 12:39:18 -0500

    rename

--- gera/wsgi.py
+++ gera/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sederia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gera.settings')
 
 application = get_wsgi_application()
